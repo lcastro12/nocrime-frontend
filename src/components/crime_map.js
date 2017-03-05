8660ce09dfd25f574b9809d7e5689deff7c1e055
LuisMesa <la.mesa10@uniandes.edu.co>
2017-03-05 18:40:50 -0500

    Se añade libreria para el mapa a package.json

--- src/components/crime_map.js
+++ src/components/crime_map.js
@@ -1,19 +1,96 @@
 import React, {Component} from 'react';
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+
+const ClosureListenersExampleGoogleMap = withGoogleMap(props => (
+    <GoogleMap
+        defaultZoom={15}
+
+        defaultCenter={new google.maps.LatLng(4.60, -74.06)}
+    >
+
+      {props.markers.map((marker, index) =>
+      {
+        const onClick = () => props.onMarkerClick(marker);
+        const onCloseClick = () => props.onCloseClick(marker);
+
+        return (
+            <Marker
+                key={index}
+                position={marker.position}
+                title={(index + 1).toString()}
+                onClick={onClick}
+            >
+              {marker.showInfo && (
+                  <InfoWindow onCloseClick={onCloseClick}>
+                    <div>
+                      <strong>{marker.content}</strong>
+                      <br />
+                      <em>The contents of this InfoWindow are actually ReactElements.</em>
+                    </div>
+                  </InfoWindow>
+              )}
+            </Marker>
+        );
+      })}
+    </GoogleMap>
+));
 
 class CrimeMap extends Component {
-  constructor(props) {
+  constructor(props)
+  {
     super(props);
 
     this.state = {
-      markers:[]
+      markers: []
     }
   }
 
-    render() {
-        return (
-            <p>Oye mi perro!</p>
-        );
+  componentDidMount()
+  {
+    const nMarkers = [];
+
+    for (let i = 0; i < this.props.crimes.length; i++)
+    {
+      const position = new google.maps.LatLng(
+          this.props.crimes[i].latitude,
+          this.props.crimes[i].longitude
+      );
+
+      nMarkers.push({
+        position,
+        content: `This is the secret message`.split(` `)[i],
+        showInfo: false,
+      });
     }
+  console.log(nMarkers.length);
+    this.setState({markers: nMarkers});
+  }
+
+
+  render()
+  {
+    console.log(this.props.crimes.length != 0 ? this.props.crimes[0].latitude : 'Nada aún');
+    console.log(this.state.markers);
+    return (
+
+        <ClosureListenersExampleGoogleMap
+            containerElement={
+              <div style={{ height: `500px` }} />
+            }
+            mapElement={
+              <div style={{ height: `100%` }} />
+            }
+
+            markers={this.props.markers}
+
+        />
+    );
+  }
 }
 
 export default CrimeMap;
